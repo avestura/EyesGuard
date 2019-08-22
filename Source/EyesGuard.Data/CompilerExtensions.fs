39efdf2eb37a75c27c40b8b4958cebbaae8d545f
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-08-22 05:04:07 +0430

    Fix Stackoverflow

--- Source/EyesGuard.Data/CompilerExtensions.fs
+++ Source/EyesGuard.Data/CompilerExtensions.fs
@@ -6,4 +6,4 @@ open System.Runtime.InteropServices
 
 type CompilerInfo() =
     static member CompilerFilePath ([<CallerFilePath;Optional;DefaultParameterValue("")>]path:string) = path
-    static member CompilerDirectory = CompilerInfo.CompilerDirectory |> Path.GetDirectoryName
+    static member CompilerDirectory = CompilerInfo.CompilerFilePath() |> Path.GetDirectoryName
